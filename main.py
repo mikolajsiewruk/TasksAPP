abf8710b54c66b361b9a586445944ce1990abbe7
mikolajsiewruk <mikolajsiewruk22@gmail.com>
2024-02-21 00:13:41 +0100

    Poprawa kodu i funkcjonalny ekran dodawania preferencji

--- main.py
+++ main.py
@@ -39,16 +39,16 @@ class LoginWindow(Screen,TasksApp):
     def login(self):
         username=self.ids.username.text
         password=self.ids.password.text
-        sql_statement1=f'''select * from Uzytkownicy where nickname='{username}' and password='{password}';'''
+        sql_statement1=f'''select * from Users where nickname='{username}' and password='{password}';'''
         self.cursor.execute(sql_statement1)
         users=self.cursor.fetchone()
         if users:
             self.app.userID = users[0]
+            self.manager.get_screen("menu").on_preferences_set(self.app.userID)
             self.manager.current = "menu"
         else:
             error_message='Invalid credentials'
             self.app.show_error(error_message)
-
     def registration_form(self):
         self.manager.current="registration"
 class MenuWindow(Screen):
@@ -57,8 +57,10 @@ class MenuWindow(Screen):
         self.app=App.get_running_app()
         self.cursor=self.app.cursor
         self.userID=self.app.userID
+    def on_preferences_set(self, user_id):
+        self.userID = user_id
     def printid(self):
-        print(self.app.userID)
+        print(self.userID)
 class Registration(Screen,TasksApp):
     def __init__(self, **kwargs):
         super(Registration, self).__init__(**kwargs)
@@ -71,22 +73,37 @@ class Registration(Screen,TasksApp):
         surname=self.ids.surname.text
         uni=self.ids.university.text
         parameters=(nickname,password,name,surname,uni,)
-        sql_statement2 = '''Insert Into Uzytkownicy(nickname,password,Imie,Nazwisko,Uczelnia) Values(%s,%s,%s,%s,%s)'''
+        sql_statement1 = '''Insert Into Users(nickname,password,Name,Surname,University) Values(%s,%s,%s,%s,%s)'''
         try:
-            self.cursor.execute(sql_statement2,parameters)
+            self.cursor.execute(sql_statement1,parameters)
             self.connection_to_db.commit()
+            sql_statement2=f'''Select * from Users where nickname='{nickname}';'''
+            self.cursor.execute(sql_statement2)
+            results=self.cursor.fetchone()
+            self.app.userID = results[0]
             self.manager.current="setpreferences"
         except mysql.connector.IntegrityError:
             error_message = 'Nickname taken'
             self.app.show_error(error_message)
-class SetPreferences(Screen,TasksApp):
+class SetPreferences(Screen):
     def __init__(self, **kwargs):
         super(SetPreferences, self).__init__(**kwargs)
         self.app = App.get_running_app()
+        self.connection_to_db=self.app.connection_to_db
         self.cursor = self.app.cursor
     def setpreferences(self):
-        print('xd')
-
+        user_ID=self.app.userID
+        parameters=(user_ID,self.ids.ects.text,self.ids.duedate.text,self.ids.gradepercentage.text,self.ids.difficulty.text,self.ids.time_consumption.text,self.ids.likability.text,self.ids.importance.text,self.ids.sudenness.text,)
+        sql_statement='''Insert into Preferences(UserID,Ects,due_date,grade_percentage,difficulty,time_consumption,likability,importance,sudenness) values (%s,%s,%s,%s,%s,%s,%s,%s,%s) '''
+        self.cursor.execute(sql_statement,parameters)
+        self.connection_to_db.commit()
+        sql_statement1=f'''select * from Users where id={user_ID}'''
+        self.cursor.execute(sql_statement1)
+        results=self.cursor.fetchone()
+        if results:
+            self.app.userID=results[0]
+            self.manager.get_screen("menu").on_preferences_set(self.app.userID)
+            self.manager.current="menu"
 class WindowManager(ScreenManager):
     pass
 
